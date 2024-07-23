211e5179941020e01680733a2675068f5d1380b0
Alena Pavioglo <alena.pavlioglo@lafosseacademy.com>
2024-07-23 11:33:47 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Initialisation of the server
+require('dotenv').config()
+const app = require('./app')
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
